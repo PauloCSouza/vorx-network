e1e7ee4cd4313e81019fcf36aa9a7b830bfd3f2e
Paulo Souza <paulos@portasabertas.org.br>
2023-04-25 18:31:18 -0300

    create subnets private and public

--- network.tf
+++ network.tf
@@ -6,6 +6,46 @@ resource "aws_vpc" "vorx-vpc-prod" {
   }
 }
 
+resource "aws_subnet" "vorx-subnet-pub-1a" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name" = "prod-public-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1a" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name" = "prod-private-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-pub-1b" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name" = "prod-public-subnet-1b"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1b" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.20.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name" = "prod-private-subnet-1b"
+  }
+}
+
 ## OUTPUT ##
 output "vpc_vorx_prod_id" {
   value = aws_vpc.vorx-vpc-prod.id
@@ -13,4 +53,20 @@ output "vpc_vorx_prod_id" {
 
 output "vpc_vorx_prod_arn" {
   value = aws_vpc.vorx-vpc-prod.arn
+}
+
+output "subnet_public_1a_id" {
+  value = aws_subnet.vorx-subnet-pub-1a.id
+}
+
+output "subnet_private_1a_id" {
+  value = aws_subnet.vorx-subnet-priv-1a.id
+}
+
+output "subnet_public_1b_id" {
+  value = aws_subnet.vorx-subnet-pub-1b.id
+}
+
+output "subnet_private_1b_id" {
+  value = aws_subnet.vorx-subnet-priv-1b.id
 }
